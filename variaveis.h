0442b39df299b54ed560e97cf08639f76229d116
Vitor Nere <vitor.nere@hotmail.com>
2015-12-04 19:47:38 -0200

    Reestruturado arquitetura do otimizador

---
+++ variaveis.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lista.h"
+
+#ifndef LISTA_H
+#define LISTA_H
+
+int how_many_times(header *fixed_header, list *variable);
+void variable_not_declarated_alone(list *in_element, header *fixed_header);
+void variable_not_declarated_middle(list *in_element, header *fixed_header);
+void variable_not_declarated_tail(list *in_element, header *fixed_header);
+
+#endif
